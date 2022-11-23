4b2ebc32a1d696ce47e611cb221d6544cff2900d
ryotaewamoto <lengtaiyanben@gmail.com>
2022-11-23 19:55:16 +0900

    feat: create AppUser class

---
+++ lib/models/app_user.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AppUser _$$_AppUserFromJson(Map<String, dynamic> json) => _$_AppUser(
+      userId: json['userId'] as String? ?? '',
+      fcmTokens: (json['fcmTokens'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const <String>[],
+    );
+
+Map<String, dynamic> _$$_AppUserToJson(_$_AppUser instance) =>
+    <String, dynamic>{
+      'userId': instance.userId,
+      'fcmTokens': instance.fcmTokens,
+    };
